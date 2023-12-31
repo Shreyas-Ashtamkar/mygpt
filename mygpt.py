270236e83593fea75cd027d335beb279ae1480be
Shreyas Ashtamkar <51398602+Shreyas-Ashtamkar@users.noreply.github.com>
2023-12-31 10:02:01 +0000

    Default System Message, ++fun, ++accuracy

--- mygpt.py
+++ mygpt.py
@@ -12,8 +12,10 @@ st.title("Shreyas MyGPT")
 
 # Initialize chat history
 if "messages" not in st.session_state:
-    # st.session_state.messages = [{"role": "system", "content": "You are an intellient chatbot specifically trained in the genere of laptops, and laptops only. You are not allowed to answer any questions apart from those related to your expertise, which is laptops."}]
-    st.session_state.messages = []
+    #st.session_state.messages = [{"role": "system", "content": "You are an intellient chatbot specifically trained in the genere of laptops, and laptops only. You are not allowed to answer any questions apart from those related to your expertise, which is laptops."}]
+    st.session_state.messages = [{"role": "system", "content":"You are an intelligent bot named Chitti. Your task is to answer the questions users may have. You are not allowed to respond to any name other than Chitti. Each answer you give should be interesting, and should contain one relevant example and one relevant joke. If you don't know the ansser to something, you should give a list of 3 capabilities you need to give the answer to asked questions. Be as creative as possible, balancing language to be simple, understandable and keep your answers the most accurate."}]
+
+    #st.session_state.messages = []
 
 for message in filter(lambda x: x['role'] != 'system', st.session_state.messages):
     with st.chat_message(message["role"]):
@@ -40,4 +42,4 @@ if prompt := st.chat_input("Type Here..."):
         # Add assistant response to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
 
-# st.write(st.session_state.messages.__str__())
+# st.write(st.session_state.messages.__str__())i
