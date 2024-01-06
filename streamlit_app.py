9c63e96f1edea477796d345d5e2cd00d40a23eba
Shreyas-Ashtamkar <shreyu@programmer.net>
2024-01-06 14:50:38 +0530

    Sidebar, downgrade to gpt3

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+from utils import *
+import streamlit as st
+import pandas as pd
+
+st.title("Chitti GPT")
+
+if not checkConnectivity():
+    st.warning("System offline. Please connect to internet and refresh.")
+    st.stop()
+
+personas = {row["Name"]:row["Prompt"] for row in pd.read_csv("persona.csv").to_dict("records")}
+
+with st.sidebar: 
+    st.title("Chitti")
+    st.header("Customizations :")
+    prompt = st.radio("Choose", options=personas.keys())
+    # INITIAL_PROMPT = 
+    # msg = st.text_input("Initial Prompt", INITIAL_PROMPT)
+
+if "messages" not in st.session_state:
+    init()
+
+for message in filter(lambda x: x['role'] != 'system', st.session_state.messages):
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Type Here..."):
+    newMessage("user", prompt)
+
+    with st.spinner('Thinking...'):
+        newMessage("assistant", generateResponse())
---
+++ utils.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import openai
+
+import requests as _requests
+
+def checkConnectivity():
+    try:
+        _requests.get("https://api.openai.com")
+        return True
+    except Exception as e:
+        return False
+
+try:
+    if checkConnectivity():
+        openai.api_key = st.secrets["OPENAI_API_KEY"]
+        ai_client = openai.AsyncOpenAI()
+    else:
+        ConnectionError("Not connected to internet")
+except Exception as e: 
+    print("Offline")
+    
+INITIAL_PROMPT = ""
+
+def newMessage(role, msg):
+    global st
+    
+    if role!="system" and (len(st.session_state.messages)>1):
+        with st.chat_message(role):
+            st.markdown(msg)
+    
+    st.session_state.messages.append({
+        "role" : role,
+        "content": msg
+    })
+
+def generateResponse():
+    print("checkConnectivity : ", checkConnectivity())
+    if not checkConnectivity(): 
+        return "The system is in Offline Mode. Cannot generate responses."
+    else:
+        try:
+            return openai.chat.completions.create(
+                model="gpt-3.5-turbo",
+                # model="gpt-4-1106-preview",
+                messages=st.session_state.messages
+            ).choices[0].message.content
+        except Exception as e:
+            print("Exception Caught :", e)
+            return "Some internal Error Occured : utils:45"
+
+def init(msg:str = "You are an intelligent bot named Chitti. Your task is to answer the questions users may have. You are not allowed to respond to any name other than Chitti. Each answer you give should be interesting, if conceptual, then should contain one relevant example and sometimes a relevant joke (only appropriate places), keep it fun and engaging. If you can't answer a question, you are not to apologise, unless explicitly stated, and you should give a list of 3 capabilities you need that will enable you to answer them. Be as creative as possible, balancing language to be simple, understandable and keep your answers the most accurate. You need to ask the name of the user and engage them in the conversation, using their first name. When they say something you don't know or agree about, create your own story. Now, introduce yourself in about 10 to 15 words."):
+    st.session_state.messages = []
+    
+    newMessage("system", msg)
+    newMessage("assistant", generateResponse())
